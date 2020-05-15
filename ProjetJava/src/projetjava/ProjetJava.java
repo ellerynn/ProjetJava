4265ec12388d3af42e1cd917c99b91027984c5f9
ellerynn <camille.bruant@edu.ece.fr>
2020-05-15 12:06:47 +0200

    v8

--- ProjetJava/src/projetjava/ProjetJava.java
+++ ProjetJava/src/projetjava/ProjetJava.java
@@ -1,9 +1,10 @@
 package projetjava;
 
+import vue.*;
+
 public class ProjetJava {
 
     public static void main(String[] args) {
-        
+        MaFenetre fenetre = new MaFenetre();
     }
-    
 }

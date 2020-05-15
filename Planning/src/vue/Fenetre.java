2e7c354ec36af65bb9916824df56f9334dfc336e
ellerynn <camille.bruant@edu.ece.fr>
2020-05-16 00:11:36 +0200

    page connexion

--- Planning/src/vue/Fenetre.java
+++ Planning/src/vue/Fenetre.java
@@ -1,3 +1,7 @@
+/*SOURCES :
+*http://www.codeurjava.com/2015/05/comment-dimensionner-fenetre-selon-ecran.html
+*https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/23108-creez-votre-premiere-fenetre
+*/
 package vue;
 
 import java.awt.*;
@@ -5,7 +9,6 @@ import java.awt.event.*;
 import javax.swing.*; 
 
 public class Fenetre extends JFrame {  
-    private JButton bouton = new JButton("Mon bouton");
     /*A CardLayout object is a layout manager for a container. It treats each component in the container as a 
     card. Only one card is visible at a time, and the container acts as a stack of cards. The first component 
     added to a CardLayout object is the visible component when the container is first displayed.
@@ -14,18 +17,16 @@ public class Fenetre extends JFrame {
     to show a specified card. The addLayoutComponent(java.awt.Component, java.lang.Object) method can be used 
     to associate a string identifier with a given card for fast random access.
     */
-    private CardLayout cl = new CardLayout();
+    private CardLayout c = new CardLayout();
     private JPanel content = new JPanel();
     //Liste des noms de nos conteneurs pour la pile de cartes
-    private String[] listContent = {"CARD_1", "CARD_2", "CARD_3"};
+    private String[] listContent = {"FormConnexion", "CARD_2", "CARD_3"};
     private int indice = 0;
     
     /*Constructeur par défaut*/      
     public Fenetre() { 
-        //http://www.codeurjava.com/2015/05/comment-dimensionner-fenetre-selon-ecran.html 
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        this.setSize (screenSize.width*2/3, screenSize.height*2/3); // donne une taille en hauteur et largeur à la fenêtre           
-        //https://openclassrooms.com/fr/courses/26832-apprenez-a-programmer-en-java/23108-creez-votre-premiere-fenetre
+        this.setSize(screenSize.width*2/3, screenSize.height*2/3); // donne une taille en hauteur et largeur à la fenêtre           
         //Nous demandons maintenant à notre objet de se positionner au centre
         this.setLocationRelativeTo(null);
         //Termine le processus lorsqu'on clique sur la croix rouge
@@ -35,8 +36,8 @@ public class Fenetre extends JFrame {
         this.setTitle("CardLayout");
 
         //On crée trois conteneurs de couleur différente
-        JPanel card1 = new JPanel();
-        card1.setBackground(Color.blue);		
+        FormConnexion connexion = new FormConnexion();
+        
         JPanel card2 = new JPanel();
         card2.setBackground(Color.red);		
         JPanel card3 = new JPanel();
@@ -47,7 +48,7 @@ public class Fenetre extends JFrame {
         //Définition de l'action du bouton
         bouton.addActionListener((ActionEvent event) -> {
             //Via cette instruction, on passe au prochain conteneur de la pile
-            cl.next(content);
+            c.next(content);
         });
 
         JButton bouton2 = new JButton("Contenu par indice");
@@ -56,15 +57,15 @@ public class Fenetre extends JFrame {
             if(++indice > 2)
                 indice = 0;
             //Via cette instruction, on passe au conteneur correspondant au nom fourni en paramètre
-            cl.show(content, listContent[indice]);
+            c.show(content, listContent[indice]);
         });
 
         boutonPane.add(bouton);
         boutonPane.add(bouton2);
         //On définit le layout
-        content.setLayout(cl);
+        content.setLayout(c);
         //On ajoute les cartes à la pile avec un nom pour les retrouver
-        content.add(card1, listContent[0]);
+        content.add(connexion, listContent[0]);
         content.add(card2, listContent[1]);
         content.add(card3, listContent[2]);
 
--- Planning/src/vue/FormConnexion.java
+++ Planning/src/vue/FormConnexion.java
@@ -1,20 +1,86 @@
 package vue;
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-public class FormConnexion {
+public class FormConnexion extends JPanel {
+   //Attributs                    
     private JButton bouton;
-    private JPanel content;
+    private JPanel champs;
     private JTextField email;
-    private JTextField password;
+    private JLabel label1;
+    private JLabel label2;
+    private JLabel label3;
+    private JPasswordField password;
     
-    /*Constructeur par défaut*/      
-    public FormConnexion() { 
+    public FormConnexion() {
+        initialisation();
+    }
+    
+    public void initialisation() {
+        label1 = new JLabel("Connexion");
+        champs = new JPanel();
+        label2 = new JLabel("Identifiant");
+        email = new JTextField();
+        label3 = new JLabel("Mot de passe");
+        password = new JPasswordField();
         bouton = new JButton("Se connecter");
-        content = new JPanel();
-        email = new JTextField(4); //4 colonnes
-        password = new JTextField(4); //4 colonnes
-    }  
+        
+        label2.setHorizontalAlignment(SwingConstants.LEFT);
+        label3.setHorizontalAlignment(SwingConstants.LEFT);
+        
+        GroupLayout c1 = new GroupLayout(champs);
+        champs.setLayout(c1);
+        c1.setHorizontalGroup(
+            c1.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addComponent(email)
+            .addGroup(c1.createSequentialGroup()
+                .addGroup(c1.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(label2)
+                    .addComponent(label3))
+                .addGap(0, 211, Short.MAX_VALUE))
+            .addComponent(password)
+        );
+        
+        c1.setVerticalGroup(
+            c1.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(c1.createSequentialGroup()
+                .addComponent(label2)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(email, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(label3)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(password, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+        );
+        
+        GroupLayout c2 = new GroupLayout(this);
+        this.setLayout(c2);
+        c2.setHorizontalGroup(
+            c2.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(c2.createSequentialGroup()
+                .addContainerGap(88, Short.MAX_VALUE)
+                .addComponent(champs, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(88, Short.MAX_VALUE))
+            .addGroup(c2.createSequentialGroup()
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(label1)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(GroupLayout.Alignment.TRAILING, c2.createSequentialGroup()
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(bouton)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        c2.setVerticalGroup(
+            c2.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(c2.createSequentialGroup()
+                .addContainerGap(52, Short.MAX_VALUE)
+                .addComponent(label1)
+                .addGap(18, 18, 18)
+                .addComponent(champs, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(bouton)
+                .addContainerGap(97, Short.MAX_VALUE))
+        );
+    }
 }

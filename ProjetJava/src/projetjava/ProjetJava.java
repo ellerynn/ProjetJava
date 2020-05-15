e245d69519a012669b987f92bd5febf90abbc8ec
ellerynn <camille.bruant@edu.ece.fr>
2020-05-15 12:56:24 +0200

    warning ?

--- ProjetJava/src/projetjava/ProjetJava.java
+++ ProjetJava/src/projetjava/ProjetJava.java
@@ -1,10 +1,19 @@
 package projetjava;
 
+import java.sql.SQLException;
 import vue.*;
 
 public class ProjetJava {
-
     public static void main(String[] args) {
-        MaFenetre fenetre = new MaFenetre();
+        //MaFenetre fenetre = new MaFenetre();
+        try {
+            Connexion c = new Connexion("edt","root","");
+        }
+        catch(SQLException sql){
+           System.out.println("exception");
+        }
+        catch(ClassNotFoundException tm) {
+           System.out.println("class exception");
+        }
     }
 }
